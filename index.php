d5aae32f03097aead2f208ff38d3ecfff2a65b21
JaViKo500 <javikogutierrez64@gmail.com>
2024-02-28 00:56:09 -0500

    add loop filter

--- index.php
+++ index.php
@@ -11,6 +11,31 @@
    add_filter('the_content', 'addRelatedPost');
 
    function addRelatedPost( $content) {
+      if( !is_singular('post') ){
+         return $content;
+      }
+      $categories = get_the_terms(get_the_ID(), 'category');
+      $categoriesIds = array();
+      foreach ($categories as $category) {
+         $categoriesIds[] = $category->term_id;
+      }
+
+      $loop = new WP_Query(array(
+         'category_in' => $categoriesIds,
+         'post_per_page' => 4,
+         'post_not_in' => array( get_the_ID() ),
+         'orderby' => 'rand'
+      ));
+
+      if ( $loop->have_posts()) {
+         $content .= 'Related posts: <br/> <ul>';
+         while ( $loop->have_posts()) {
+            $loop->the_post();
+            $content .= '<li><a href="'.get_permalink().'">'.get_the_title().'</a></li>';
+         }
+         $content .= '</ul>';
+      }
+      wp_reset_query();
       return $content;
    }
 ?>
